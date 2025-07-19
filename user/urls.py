62698071895f3c6f62d4bf243de03dcc9d1ae3b9
swastik <swastikrai666@gmail.com>
2025-07-19 13:59:18 +0545

    nth

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('api/signup/', views.UserSignUpAPIView.as_view(), name='signupapi'),
-    path('api/login/', views.UserLogInAPIView.as_view(),name='loginapi'),
-    path('api/forgetpw/',views.ForgetpwAPI.as_view(),name='forgetpwapi')
+    path('signup/', views.UserSignUpAPIView.as_view(), name='signupapi'),
+    path('login/', views.UserLogInAPIView.as_view(),name='loginapi'),
+    path('forgetpw/',views.ForgetpwAPI.as_view(),name='forgetpwapi')
 ]
