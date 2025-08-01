4fa053929754b2759cdb9dad1dcd6a7ee1910a51
swastik <swastikrai666@gmail.com>
2025-08-01 18:10:35 +0545

    JWT authentication is added to login and properties

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('signup/', views.UserSignUpAPIView.as_view(), name='signupapi'),
-    path('login/', views.UserLogInAPIView.as_view(),name='loginapi'),
+    path('signup/', views.user_signup_view, name='signupapi'),
+    path('login/', views.user_login_view,name='loginapi'),
     path('forgetpw/',views.ForgetpwAPI.as_view(),name='forgetpwapi')
 ]
